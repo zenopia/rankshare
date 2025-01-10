01db484cb95718af265726d16b588e32af19186e
Zenopia <zenopia@gmail.com>
2025-01-10 19:22:46 +0400

    fix: server logs

--- ecosystem.config.dev.js
+++ ecosystem.config.dev.js
@@ -6,6 +6,16 @@ module.exports = {
     env: {
       PORT: 3001,
       NODE_ENV: 'production'
-    }
+    },
+    error_file: './logs/err.log',
+    out_file: './logs/out.log',
+    log_file: './logs/combined.log',
+    time: true,
+    instances: 1,
+    exec_mode: 'fork',
+    max_memory_restart: '1G',
+    watch: false,
+    kill_timeout: 3000,
+    wait_ready: true
   }]
 }; 
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,17 @@ module.exports = {
       env: {
         PORT: 3000,
         NODE_ENV: 'production'
-      }
+      },
+      error_file: './logs/err.log',
+      out_file: './logs/out.log',
+      log_file: './logs/combined.log',
+      time: true,
+      instances: 1,
+      exec_mode: 'fork',
+      max_memory_restart: '1G',
+      watch: false,
+      kill_timeout: 3000,
+      wait_ready: true
     }
   ]
 } 
