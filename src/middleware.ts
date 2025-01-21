e944531a8bda9bdda6bc404708e4f1f576bbf057
Zenopia <zenopia@gmail.com>
2025-01-21 21:14:58 +0400

    fix: auth redirect 4

--- src/middleware.ts
+++ src/middleware.ts
@@ -137,8 +137,14 @@ export default authMiddleware({
 
 export const config = {
   matcher: [
-    "/((?!.+\\.[\\w]+$|_next|sign-in|sign-up).*)",
-    "/(api|trpc)(.*)",
-    "/@:username*"
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - public folder
+     */
+    "/((?!_next/static|_next/image|favicon.ico).*)",
+    "/(api|trpc)(.*)"
   ]
 };
