49775118fdd8bd9c92d5677869ab20ff12909a48
Zenopia <zenopia@gmail.com>
2025-01-22 11:33:19 +0400

    fix: CSP blocking clerk production

--- next.config.js
+++ next.config.js
@@ -22,6 +22,19 @@ const nextConfig = {
   },
   experimental: {
     webpackBuildWorker: false
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://*.clerk.dev https://*.clerk.com https://*.clerk.accounts.dev https://*.favely.net;"
+          }
+        ]
+      }
+    ];
   }
 };
 
